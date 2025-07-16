94e23f56ac51bc04d90e28a86194cfb93dd35070
Manan Shah <131394759+mananshah3923@users.noreply.github.com>
2025-07-16 16:35:56 -0400

    Update postinstall.ts

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -32,8 +32,8 @@ async function main() {
       console.error("File based MCP config is not supported on Vercel.");
       process.exit(1);
     }
-    console.log("Running on Vercel, performing database migration.");
-    await runCommand("yarn db:migrate", "Database migration");
+    // console.log("Running on Vercel, performing database migration.");
+    // await runCommand("yarn db:migrate", "Database migration");
   } else if (IS_DOCKER_ENV) {
     if (FILE_BASED_MCP_CONFIG) {
       console.error("File based MCP config is not supported in Docker.");
